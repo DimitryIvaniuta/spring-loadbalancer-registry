4a2f5e8ee1e92be38b905491f7fa1dac7e3b4013
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2026-01-09 22:05:35 +0100

    LoadBalancer postgres testcontainer configuration

---
+++ loadbalancer-lib/src/test/java/com/github/dimitryivaniuta/loadbalancer/infra/PostgresTcConfig.java
@@ -0,0 +1,27 @@
+package com.github.dimitryivaniuta.loadbalancer.infra;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration
+public class PostgresTcConfig {
+
+    static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:16-alpine")
+            .withDatabaseName("lb")
+            .withUsername("lb")
+            .withPassword("lb");
+
+    static {
+        POSTGRES.start();
+    }
+
+    @DynamicPropertySource
+    static void props(DynamicPropertyRegistry r) {
+        r.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        r.add("spring.datasource.username", POSTGRES::getUsername);
+        r.add("spring.datasource.password", POSTGRES::getPassword);
+        r.add("spring.datasource.driver-class-name", POSTGRES::getDriverClassName);
+    }
+}

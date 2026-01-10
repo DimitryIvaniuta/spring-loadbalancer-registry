cb82eb5ea45c24d3c9efb59fcb916f3e53ed9833
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2026-01-10 21:39:18 +0100

    Load balancer abstract testcontainer

---
+++ loadbalancer-lib/src/test/java/com/github/dimitryivaniuta/loadbalancer/infra/AbstractPostgresIT.java
@@ -0,0 +1,25 @@
+package com.github.dimitryivaniuta.loadbalancer.infra;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class AbstractPostgresIT {
+
+    @Container
+    static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:16-alpine")
+            .withDatabaseName("lb")
+            .withUsername("lb")
+            .withPassword("lb");
+
+    @DynamicPropertySource
+    static void dbProps(DynamicPropertyRegistry r) {
+        r.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        r.add("spring.datasource.username", POSTGRES::getUsername);
+        r.add("spring.datasource.password", POSTGRES::getPassword);
+        r.add("spring.datasource.driver-class-name", POSTGRES::getDriverClassName);
+    }
+}

c021c260c80045c2ff6adc9ed0ba2c966a6cf9de
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2026-01-10 21:35:38 +0100

    Load balancer Instance entity

---
+++ loadbalancer-lib/src/main/java/com/github/dimitryivaniuta/loadbalancer/domain/LbInstance.java
@@ -0,0 +1,26 @@
+package com.github.dimitryivaniuta.loadbalancer.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "lb_instance")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LbInstance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "address", nullable = false, length = 512, unique = true)
+    private String address;
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
+}

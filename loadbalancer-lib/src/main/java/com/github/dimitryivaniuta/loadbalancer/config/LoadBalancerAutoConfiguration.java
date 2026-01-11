83e28ac437ad32af4172b59b0dd7ddd63556e0b9
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2026-01-11 23:00:55 +0100

    Load Balancer Auto Configuration strategy updates

--- loadbalancer-lib/src/main/java/com/github/dimitryivaniuta/loadbalancer/config/LoadBalancerAutoConfiguration.java
+++ loadbalancer-lib/src/main/java/com/github/dimitryivaniuta/loadbalancer/config/LoadBalancerAutoConfiguration.java
@@ -1,7 +1,9 @@
 package com.github.dimitryivaniuta.loadbalancer.config;
 
 import com.github.dimitryivaniuta.loadbalancer.api.LoadBalancingStrategy;
-import com.github.dimitryivaniuta.loadbalancer.api.RoundRobinStrategy;
+import com.github.dimitryivaniuta.loadbalancer.repo.LbCursorRepository;
+import com.github.dimitryivaniuta.loadbalancer.service.DbCursorService;
+import com.github.dimitryivaniuta.loadbalancer.strategy.*;
 import com.github.dimitryivaniuta.loadbalancer.domain.LbDecision;
 import com.github.dimitryivaniuta.loadbalancer.domain.LbInstance;
 import com.github.dimitryivaniuta.loadbalancer.repo.LbDecisionRepository;
@@ -11,12 +13,15 @@ import com.github.dimitryivaniuta.loadbalancer.service.LoadBalancerService;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.time.Clock;
 
 @AutoConfiguration
 @EnableConfigurationProperties(LoadBalancerProperties.class)
@@ -37,18 +42,45 @@ public class LoadBalancerAutoConfiguration {
         return new DecisionContext(); // or inject deps if it has any
     }
 
-/*    @Bean
-    public LoadBalancingStrategy loadBalancingStrategy() {
+    @Bean
+    @ConditionalOnProperty(prefix = "loadbalancer", name = "strategy", havingValue = "RANDOM")
+    public LoadBalancingStrategy randomStrategy() {
+        return new RandomStrategy();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "loadbalancer", name = "strategy", havingValue = "WEIGHTED_RANDOM")
+    public LoadBalancingStrategy weightedRandomStrategy(LoadBalancerProperties props) {
+        return new WeightedRandomStrategy(props);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "loadbalancer", name = "strategy", havingValue = "DISTRIBUTED_ROUND_ROBIN")
+    public LoadBalancingStrategy distributedRoundRobinStrategy(DbCursorService cursor) {
+        return new DistributedRoundRobinStrategy(cursor);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(LoadBalancingStrategy.class)
+    public LoadBalancingStrategy defaultRoundRobinStrategy() {
         return new RoundRobinStrategy();
     }
 
     @Bean
-    public LoadBalancerService loadBalancerService(
-            LbInstanceRepository repo,
-            LoadBalancingStrategy strategy,
-            LoadBalancerProperties props,
-            LbDecisionRepository decisionRepo
-    ) {
-        return new LoadBalancerService(repo, strategy, props, decisionRepo);
-    }*/
+    @ConditionalOnMissingBean
+    public DbCursorService dbCursorService(LbCursorRepository repo) {
+        return new DbCursorService(repo);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Clock loadBalancerClock() {
+        return Clock.systemUTC();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "loadbalancer", name = "strategy", havingValue = "LEAST_RECENTLY_USED")
+    public LoadBalancingStrategy leastRecentlyUsedStrategy(LbInstanceRepository repo, Clock clock) {
+        return new LeastRecentlyUsedStrategy(repo, clock);
+    }
 }

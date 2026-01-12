b8b86668da5c37e3005f7932aaa7ee23a570b05a
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2026-01-12 18:38:15 +0100

    Load Balancer Properties config

--- loadbalancer-lib/src/main/java/com/github/dimitryivaniuta/loadbalancer/config/LoadBalancerProperties.java
+++ loadbalancer-lib/src/main/java/com/github/dimitryivaniuta/loadbalancer/config/LoadBalancerProperties.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "loadbalancer")
@@ -12,4 +15,22 @@ public class LoadBalancerProperties {
      * Max number of registered addresses allowed.
      */
     private int maxInstances = 10;
+
+    private Strategy strategy = Strategy.ROUND_ROBIN;
+
+    /**
+     * Used by WEIGHTED_RANDOM. Default weight is 1 if not present.
+     * Example:
+     * loadbalancer.weights.http://10.0.0.1:8080=5
+     * loadbalancer.weights.http://10.0.0.2:8080=1
+     */
+    private Map<String, Integer> weights = new HashMap<>();
+
+    public enum Strategy {
+        ROUND_ROBIN,
+        DISTRIBUTED_ROUND_ROBIN,
+        RANDOM,
+        WEIGHTED_RANDOM,
+        LEAST_RECENTLY_USED
+    }
 }
